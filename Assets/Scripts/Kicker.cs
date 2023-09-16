af033d26d92233d9f9ce2388296ec73f972724a2
DarioLoll <stromajer.dario@gmail.com>
2023-09-16 22:35:45 +0200

    Initial commit

---
+++ Assets/Scripts/Kicker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kicker : MonoBehaviour
+{
+    Transform player;
+    PlayerController playerController;
+
+    [SerializeField]
+    float kickStrength = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.parent;
+        playerController = player.GetComponent<PlayerController>();
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ball" && playerController.IsKicking)
+        {
+            Vector2 kickDirection = (transform.position - collision.gameObject.transform.position).normalized;
+            collision.rigidbody.AddForce(-kickDirection * kickStrength, ForceMode2D.Impulse);
+        }
+            
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private bool isLeft = false;
+
+    bool isGrounded = false;
+
+    public bool IsLeft
+    {
+        get { return isLeft; }
+        set { isLeft = value; }
+    }
+
+    public bool IsKicking
+    {
+        get => (IsLeft && inputActions.MovementLeft.Kick.IsInProgress()) ||
+                !IsLeft && inputActions.MovementRight.Kick.IsInProgress();
+    }
+
+    [SerializeField]
+    private float jumpForce = 7f;
+
+    [SerializeField]
+    private float moveSpeed = 4f;
+
+    [SerializeField]
+    private float kickStrength = 2f;
+
+    private Rigidbody2D rigidBody;
+    private InputActions inputActions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        inputActions = new InputActions();
+        if (IsLeft)
+        {
+            inputActions.MovementLeft.Enable();
+            inputActions.MovementLeft.Jump.performed += Jump;
+        }
+        else
+        {
+            inputActions.MovementRight.Enable();
+            inputActions.MovementRight.Jump.performed += Jump;
+        }
+
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+        {
+            rigidBody.AddForce(new Vector2(0, jumpForce * rigidBody.mass), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float input;
+        if (inputActions.MovementLeft.enabled)
+            input = inputActions.MovementLeft.Move.ReadValue<float>();
+        else
+            input = inputActions.MovementRight.Move.ReadValue<float>();
+
+        rigidBody.velocity = new Vector2(input * moveSpeed, rigidBody.velocity.y);
+    }
+
+    private void Update()
+    {
+        Transform boot = transform.GetChild(0);
+        if ((inputActions.MovementLeft.Kick.inProgress && IsLeft) || (inputActions.MovementRight.Kick.inProgress && !IsLeft))
+        {
+            if (boot.localRotation.eulerAngles.z < 90)
+            {
+                boot.Rotate(0, 0, Time.deltaTime * kickStrength);
+            }
+        }
+        else if (boot.localRotation.eulerAngles.z > 0)
+        {
+            if (boot.localRotation.eulerAngles.z > 180)
+                boot.Rotate(0,0, 360 - boot.localRotation.eulerAngles.z);
+            else
+                boot.Rotate(0,0, -Time.deltaTime * kickStrength);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3 /*Ground*/)
+        {
+            isGrounded = true;
+        }
+    }
+
+
+}
